23d63be097a9b8acda99ac5a70c15ddc8d61d507
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-07-25 21:19:43 +0200

    feat: add error handling to methods refactor: rename shadowing variables

--- KinematicsSolverApp/fk_solver.py
+++ KinematicsSolverApp/fk_solver.py
@@ -18,96 +18,136 @@ class FkSolver:
         :param link4: "L" effector dimension
         :param link5: "H" effector dimension
         """
+        try:
+            if not isinstance(link1, int) or not isinstance(link2, int) or not isinstance(link3, int) or not isinstance(
+                    link4, int) or not isinstance(link5, int):
+                status = "Links dimensions must be integers"
+                print(status)
+                self.link1 = None
+                self.link2 = None
+                self.link3 = None
+                self.link4 = None
+                self.link5 = None
+            else:
+                if link1 < 0 or link2 < 0 or link3 < 0 or link4 < 0 or link5 < 0:
+                    status = "Links dimensions must be equal or greater then 0"
+                    print(status)
+                    self.link1 = None
+                    self.link2 = None
+                    self.link3 = None
+                    self.link4 = None
+                    self.link5 = None
+                else:
+                    status = "Links dimensions ok"
+                    print(status)
+                    self.link1 = link1
+                    self.link2 = link2
+                    self.link3 = link3
+                    self.link4 = link4
+                    self.link5 = link5
+        except TypeError as error:
+            print(str(error))
 
-        self.link1 = link1
-        self.link2 = link2
-        self.link3 = link3
-        self.link4 = link4
-        self.link5 = link5
-
-    def dh(self, theta1: float, theta2: float, theta3: float, theta4: float) -> np.array:
+    def dh(self, theta1: float, theta2: float, theta3: float, theta4: float) -> Tuple[np.array, str]:
         """
         Method creates dh table with the given parameters.\n
         :param theta1: Base rotation angle
         :param theta2: 1st link rotation angle
         :param theta3: 2nd link rotation angle
         :param theta4: 3rd link rotation angle
-        :return: dh_table
+        :return: dh_table, status
         """
+        try:
+            if not isinstance(theta1, float) or not isinstance(theta2, float) or not isinstance(theta3,
+                                                                                                float) or not isinstance(
+                    theta4, float):
+                raise TypeError("Thetas values must be float")
+            else:
+                # Convert angels in degrees to radians
+                theta1 = math.radians(theta1)
+                theta2 = math.radians(theta2)
+                theta3 = math.radians(theta3)
+                theta4 = math.radians(theta4)
 
-        # Convert angels in degrees to radians
-        theta1 = math.radians(theta1)
-        theta2 = math.radians(theta2)
-        theta3 = math.radians(theta3)
-        theta4 = math.radians(theta4)
+                # DH table with input parameters
+                table_dh = np.array([[theta1, self.link1, 0, math.radians(90)],
+                                     [theta2, 0, self.link2, 0],
+                                     [theta3, 0, self.link3, 0],
+                                     [theta4, 0, self.link4, 0],
+                                     [math.radians(-90), 0, self.link5, 0]])
+                if None in table_dh:
+                    raise TypeError("Robot configurations not defined correctly")
+                else:
+                    status = "DH table generated correctly"
+                    return table_dh, status
 
-        # DH table with input parameters
-        table_dh = np.array([[theta1, self.link1, 0, math.radians(90)],
-                             [theta2, 0, self.link2, 0],
-                             [theta3, 0, self.link3, 0],
-                             [theta4, 0, self.link4, 0],
-                             [math.radians(-90), 0, self.link5, 0]])
-        return table_dh
+        except TypeError as status:
+            return_error = np.array([[0, 0, 0, 0],
+                                     [0, 0, 0, 0],
+                                     [0, 0, 0, 0],
+                                     [0, 0, 0, 0],
+                                     [0, 0, 0, 0]])
+            return return_error, str(status)
 
     @staticmethod
-    def hom_Matrix(table_dh, i: int) -> np.array:
+    def hom_matrix(dh_table: np.array, i: int) -> np.array:
         """
         Generation of homogenous transformation matrices Ti. \n
-        :param table_dh: Denavit–Hartenbergs table
+        :param dh_table: Denavit–Hartenbergs table
         :param i: Number of transformations
         :return: t_dh - Homogenous trans. matrix Ti
         """
 
         t_dh = np.array([
-            [math.cos(table_dh[i, 0]), (-1 * math.sin(table_dh[i, 0]) * math.cos(table_dh[i, 3])),
-             (math.sin(table_dh[i, 0]) * math.sin(table_dh[i, 3])),
-             table_dh[i, 2] * math.cos(table_dh[i, 0])],
-            [math.sin(table_dh[i, 0]), (math.cos(table_dh[i, 0]) * math.cos(table_dh[i, 3])),
-             (-1 * math.cos(table_dh[i, 0]) * math.sin(table_dh[i, 3])),
-             table_dh[i, 2] * math.sin(table_dh[i, 0])],
-            [0, math.sin(table_dh[i, 3]), math.cos(table_dh[i, 3]), table_dh[i, 1]],
+            [math.cos(dh_table[i, 0]), (-1 * math.sin(dh_table[i, 0]) * math.cos(dh_table[i, 3])),
+             (math.sin(dh_table[i, 0]) * math.sin(dh_table[i, 3])),
+             dh_table[i, 2] * math.cos(dh_table[i, 0])],
+            [math.sin(dh_table[i, 0]), (math.cos(dh_table[i, 0]) * math.cos(dh_table[i, 3])),
+             (-1 * math.cos(dh_table[i, 0]) * math.sin(dh_table[i, 3])),
+             dh_table[i, 2] * math.sin(dh_table[i, 0])],
+            [0, math.sin(dh_table[i, 3]), math.cos(dh_table[i, 3]), dh_table[i, 1]],
             [0, 0, 0, 1]])
         return t_dh
 
     @staticmethod
-    def solver(table_dh) -> Tuple[int, List[Tuple[float, float, float]], str]:
+    def solver(dh_table) -> Tuple[int, List[Tuple[float, float, float]], str]:
         """
         Forward kinematics end effector xyz pos. solver. \n
-        :param table_dh: Denavit–Hartenbergs table
+        :param dh_table: Denavit–Hartenbergs table
         :return: alpha; xyz_pos_link; status: Orientation and list of xyz positions of each link end
         """
-        matrix_t = FkSolver.hom_Matrix(table_dh, 0)
+        matrix_t = FkSolver.hom_matrix(dh_table, 0)
         xyz_pos1 = []
 
         # Determination of the homogenous transformation matrices
-        for number in range(1, len(table_dh)):
-            matrix_t = matrix_t @ FkSolver.hom_Matrix(table_dh, number)
+        for number in range(1, len(dh_table)):
+            matrix_t = matrix_t @ FkSolver.hom_matrix(dh_table, number)
             xyz_pos = float(round(matrix_t[0, 3])), float(round(matrix_t[1, 3])), float(round(matrix_t[2, 3]))
             xyz_pos1.append(xyz_pos)
-
         # End effector orientation
         try:
-            if round(xyz_pos1[-3][-1]) == 0:
+            if round(round(dh_table[-2][-2])) == 0:
                 raise ZeroDivisionError
             else:
                 # z-ef - "z" dimension between last links "z" dim and end effector "z" dim.
                 z_ef = round(matrix_t[2, 3]) - round(xyz_pos1[-3][-1])
                 # alpha - z_ef / links4_length
-                alpha = math.degrees(math.asin(z_ef / round(table_dh[-2][-2])))
+                alpha = math.degrees(math.asin(z_ef / round(dh_table[-2][-2])))
                 status = "Forward kinematics calculations ended successfully"
                 return round(alpha), xyz_pos1, status
 
         except RuntimeError as error:
             status = str(error)
             return 0, [(0, 0, 0)], status
-        except ZeroDivisionError as error:
-            status = str(error)
+        except ZeroDivisionError:
+            status = "ZeroDivisionError: Table_dh[-2][-2] must be != 0"
             return 0, [(0, 0, 0)], status
         except:
             status = "Sth went wrong"
             return 0, [(0, 0, 0)], status
 
-    def solve_auto(self, theta1: float, theta2: float, theta3: float, theta4: float) -> Tuple[int, List[Tuple[float, float, float]], str]:
+    def solve_auto(self, theta1: float, theta2: float, theta3: float, theta4: float) -> Tuple[
+                    int, List[Tuple[float, float, float]], str]:
         """
         Calculate end effectors xyz pos. with given rotations of initialized robotic model.\n
         :param theta1: Base rotation angle
@@ -116,25 +156,25 @@ class FkSolver:
         :param theta4: 3rd link rotation angle
         :return: alpha; xyz_pos_link; status: Orientation and list of xyz positions of each link end
         """
-        table_dh = FkSolver.dh(self, theta1, theta2, theta3, theta4)
-        return FkSolver.solver(table_dh)
+        try:
+            table_dh = FkSolver.dh(self, theta1, theta2, theta3, theta4)
+            return FkSolver.solver(table_dh[0])
+        except TypeError:
+            status = "Robot configurations not defined correctly"
+            return_error = (0, [(0.0, 0.0, 0.0), (0.0, 0.0, 0.0), (0.0, 0.0, 0.0), (0.0, 0.0, 0.0)],
+                            status)
+            return return_error
 
     @staticmethod
-    def solve_user(table_dh) -> Tuple[int, List[Tuple[float, float, float]], str]:
+    def solve_user(dh_table) -> Tuple[int, List[Tuple[float, float, float]], str]:
         """
         Calculate end effectors xyz pos. with given dh table.\n
-        :param table_dh: Denavit–Hartenbergs table
+        :param dh_table: Denavit–Hartenbergs table
         :return: alpha; xyz_pos_link; status: Orientation and list of xyz positions of each link end
         """
-        return FkSolver.solver(table_dh)
+        return FkSolver.solver(dh_table)
 
 
-# test
-Robot = FkSolver(118, 150, 0, 54, 0)
-print(Robot.solve_auto(0, 90, 0, 0))
-table_dh3 = np.array([[0, 118, 0, math.radians(90)],
-                      [math.radians(90), 0, 150, 0],
-                      [0, 0, 150, 0],
-                      [0, 0, 54, 0],
-                      [math.radians(-90), 0, 0, 0]])
-print((Robot.solve_user(table_dh3)))
+robot = FkSolver(118, 150, 150, 54, 0)
+print(robot.solve_auto(0.0, 90.0, 0.0, 0.0))
+print(robot.dh(0.0, 90.0, 0.0, 0.0))
