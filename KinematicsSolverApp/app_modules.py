29bd54291eb06f47ac6901cf403f46f7bd3810a7
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-08-03 15:24:36 +0200

    feat: create module that import users and standard modules

---
+++ KinematicsSolverApp/app_modules.py
@@ -0,0 +1,22 @@
+"""
+Module imports users modules:\n
+Ui_MainWindow \n
+ui_functions \n
+app_functions \n
+
+and standard modules \n
+logging \n
+sys \n
+"""
+
+# GUI FILE
+from KinematicsSolverApp.ui.ui_KinematicsSolverApp import Ui_MainWindow
+
+# IMPORT FUNCTIONS
+from KinematicsSolverApp.ui.ui_functions import *
+
+# ==> APP FUNCTIONS
+from KinematicsSolverApp.ui.app_functions import *
+
+import logging
+
