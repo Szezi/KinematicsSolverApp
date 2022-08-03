4ecaea77a62c03a2db02642096a999538ed79eb7
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-08-03 15:23:33 +0200

    fix: change location of import icons_rc file

--- KinematicsSolverApp/ui/ui_KinematicsSolverApp.py
+++ KinematicsSolverApp/ui/ui_KinematicsSolverApp.py
@@ -12,7 +12,8 @@ from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
-import icons_rc
+from KinematicsSolverApp.ui.icons import icons_rc
+
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
