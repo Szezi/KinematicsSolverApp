a89ac96d7c1c92ea75bb88f13c8931e0a5a77e3c
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-08-16 20:22:21 +0200

    feat: add method for btn to init fk and display dh table feat: add method for btn to solve fk feat: add method for btn to init ik and display dh table feat: add method for btn to solve ik

--- KinematicsSolverApp/ui/app_functions.py
+++ KinematicsSolverApp/ui/app_functions.py
@@ -3,12 +3,16 @@ from KinematicsSolverApp.main import *
 from KinematicsSolverApp.main import MainWindow
 from KinematicsSolverApp.ui.ui_functions import *
 
+from KinematicsSolverApp.modules.robotic_arm import RoboticArm
+
 
 class AppFunctions(MainWindow):
     """ Class contains functions to control application depending on users actions """
 
     def __init__(self):
         super().__init__()
+        self.robotic_arm_ik = None
+        self.robotic_arm_fk = None
 
     def page_fk_init(self):
         """
@@ -17,7 +21,20 @@ class AppFunctions(MainWindow):
 
         :return: robotic_arm_fk
         """
-        pass
+
+        robotic_parm = UIFunctions.robotic_init_from_qtable_to_dict(self, self.ui.tableWidget_fk_input)
+
+        for key in robotic_parm:
+            for value in range(0, 3):
+                try:
+                    robotic_parm[key][value] = int(robotic_parm[key][value])
+                except ValueError:
+                    status = "Error: Values must be int"
+                    UIFunctions.log_list(self, status)
+
+        self.robotic_arm_fk = RoboticArm(robotic_parm)
+        UIFunctions.log_list(self, self.robotic_arm_fk.fk_check_input_param())
+        UIFunctions.write_to_dh_table(self)
 
     def page_fk_solve(self):
         """
@@ -26,7 +43,24 @@ class AppFunctions(MainWindow):
 
         :return: [Px, Py, Pz]
         """
-        pass
+        thetas = UIFunctions.robotic_input_fk(self)
+
+        try:
+            thetas = tuple(float(item) for item in thetas)
+        except ValueError:
+            status = "Error: Values must be float"
+            UIFunctions.log_list(self, status)
+        else:
+            self.robotic_arm_fk.fk_solve_auto(thetas[0], thetas[1], thetas[2], thetas[3])
+            result = self.robotic_arm_fk.fk_solve_auto(thetas[0], thetas[1], thetas[2], thetas[3])
+
+            UIFunctions.write_matrix_to_table(self, self.ui.tableWidget_fk_T12, 2)
+            UIFunctions.write_matrix_to_table(self, self.ui.tableWidget_fk_T13, 3)
+            UIFunctions.write_matrix_to_table(self, self.ui.tableWidget_fk_T14, 4)
+            UIFunctions.write_matrix_to_table(self, self.ui.tableWidget_fk_T15, 5)
+
+            # Display status log
+            UIFunctions.log_list(self, result[2])
 
     def page_fk_solve_user(self):
         """
@@ -35,7 +69,16 @@ class AppFunctions(MainWindow):
 
         :return: Forward kinematics
         """
-        pass
+        user_dh = UIFunctions.read_user_dh(self)
+        fk_user_result = RoboticArm.fk_solve_user(user_dh)
+        print(fk_user_result)
+        user_px = fk_user_result[1][3][0]
+        user_py = fk_user_result[1][3][1]
+        user_pz = fk_user_result[1][3][2]
+
+        self.ui.lcdNumber_fk_px.display(user_px)
+        self.ui.lcdNumber_fk_py.display(user_py)
+        self.ui.lcdNumber_fk_pz.display(user_pz)
 
     def page_ik_init(self):
         """
@@ -43,7 +86,17 @@ class AppFunctions(MainWindow):
         Initialize robotics arm model with given parameters. \n
         :return: Inverse kinematics
         """
-        pass
+        robotic_parm = UIFunctions.robotic_init_from_qtable_to_dict(self, self.ui.tableWidget_ik_input)
+        for key in robotic_parm:
+            for value in range(0, 3):
+                try:
+                    robotic_parm[key][value] = int(robotic_parm[key][value])
+                except ValueError:
+                    status = "Error: Values must be int"
+                    UIFunctions.log_list(self, status)
+
+        self.robotic_arm_ik = RoboticArm(robotic_parm)
+        UIFunctions.log_list(self, self.robotic_arm_ik.fk_check_input_param())
 
     def page_ik_solve(self):
         """
@@ -51,4 +104,24 @@ class AppFunctions(MainWindow):
         Calculate joints values of robotic arm with given xyz position. \n
         :return: [theta1, theta2, theta3, theta4]
         """
-        pass
+
+        xyz = UIFunctions.robotic_input_ik(self)
+        try:
+            xyz = tuple(int(item) for item in xyz)
+        except ValueError:
+            status = "Error: Values must be int"
+            UIFunctions.log_list(self, status)
+        else:
+            self.robotic_arm_ik.ik_solver(xyz[0], xyz[1], xyz[2], xyz[3])
+            config_1 = self.robotic_arm_ik.ik_get_config1()
+            config_2 = self.robotic_arm_ik.ik_get_config2()
+
+            # Clear lcd
+            UIFunctions.display_ik_results(self, [[0.0, 0.0, 0.0, 0.0], "clear"], [[0.0, 0.0, 0.0, 0.0], "clear"])
+
+            # Display results
+            UIFunctions.display_ik_results(self, config_1, config_2)
+
+            # Display status log
+            UIFunctions.log_list(self, config_1[1])
+            UIFunctions.log_list(self, config_2[1])
