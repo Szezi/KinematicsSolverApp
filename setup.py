ae614912b92cf1836fd38c1784af9b00d16e1be9
Daniel Sziling <97255726+Szezi@users.noreply.github.com>
2022-08-23 20:28:41 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='KinematicsSolverApp',
+    version='1.0',
+    url='https://github.com/Szezi/KinematicsSolverApp',
+    license='GNU General Public License v3.0',
+    author='Daniel Sziling',
+    description='Desktop application calculate forward and inverse kinematics of robotic arm',
+)
